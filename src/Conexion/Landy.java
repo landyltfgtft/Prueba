f17cee3540b4c19f3dac4983f7339271f9e9035a
alechos._.0k <alecsis012@hotmail.com>
2026-05-11 12:06:25 -0600

    ghv

--- src/Conexion/Landy.java
+++ src/Conexion/Landy.java
@@ -3,7 +3,7 @@ package Conexion;
 public class Landy {
 	public static void main(String [] args) {
 		System.out.println("hola dominguez");
-		System.out.println("holaaa");
+		System.out.println("holaaa sebas");
 		
 	}
 }
